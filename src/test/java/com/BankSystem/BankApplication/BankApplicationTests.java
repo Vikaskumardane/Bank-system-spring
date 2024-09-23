b8311f4faa3e841a1f25fc561ba4d926253c4fdf
Vikaskumar Dane <vikasdane06@gmail.com>
2024-09-23 22:31:18 +0530

    1st commit with spring template

---
+++ src/test/java/com/BankSystem/BankApplication/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BankSystem.BankApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
